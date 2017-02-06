50add8a28ff583f3c9d68a9fd9de089ef0c92a38
Mario Souto <mariosoutoskn@gmail.com>
2017-02-06 17:53:27 -0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,43 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const watch          = require('gulp-watch');
+const svg_sprite     = require('gulp-svg-sprite');
+const color          = require('gulp-color');
+
+
+
+// - Sprites Generator
+// - Icons
+const configSvg = {
+    mode: {
+        symbol: {
+            dest: 'sprite',
+            sprite: 'svg_sprite.svg',
+            example: true
+        }
+    },
+    svg: {
+        xmlDeclaration: false,
+        doctypeDeclaration: false
+    },
+    variables   : {
+        now     : +new Date(),
+        png     : function() {
+            return function(sprite, render) {
+                return render(sprite).split('.svg').join('.png');
+            }
+        }
+    }
+};
+
+gulp.task('sprites', function(){
+    return watch('./icons/svg/**/*.svg', { ignoreInitial: false }, function () {
+      gulp.src('./icons/svg/**/*.svg')
+      .pipe(svg_sprite(configSvg))
+      .pipe(gulp.dest('./icons'))
+      console.log(color('[SVG-ICON _sprite generated]', 'GREEN'));
+    });
+});
 
 // Static Server + watching scss/html files
 gulp.task('serve', function() {
@@ -12,4 +50,4 @@ gulp.task('serve', function() {
 });
 
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['serve', 'sprites']);
